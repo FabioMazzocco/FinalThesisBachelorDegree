7880cd490285f2a8c1a22d6ee35272d6b5e8523c
FabioMazzocco <FabioMazzocco@169.254.79.176>
2019-08-17 18:32:31 +0200

    UI Home v1.0

--- src/it/polito/s234844/thesis/Main.java
+++ src/it/polito/s234844/thesis/Main.java
@@ -20,8 +20,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("DUE DATE TOOL by Fabio Mazzocco");
 			primaryStage.show();
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
